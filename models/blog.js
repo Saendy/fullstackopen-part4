f07e6fbed331a92c6604ad67ee881070f7023f23
Saendy <jayden_saw@hotmail.com>
2023-07-17 18:54:37 +0800

    exercises 4.3-4.7

--- models/blog.js
+++ models/blog.js
@@ -7,11 +7,11 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(console.log('connected to MongoDB'))
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
+    .then(console.log('connected to MongoDB'))
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+/*
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -28,8 +28,15 @@ const personSchema = new mongoose.Schema({
     }
   }
 })
+*/
 
-
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+/*
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -37,6 +44,6 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+*/
 
-
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Blog', blogSchema)
